14f037188e3400d54ea00ff628773a3360c43df1
monysary <sary.mony@gmail.com>
2023-05-12 10:44:16 -0700

    updated user signup and login page ui and functionality

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,4 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios"
+
+import authService from "@/utils/authService";
+
 export default function Signup({ toggleHidden, setToggleHidden }) {
+    // Handle new user signup form
+    const [signupForm, setSignupForm] = useState({
+        firstName: '',
+        lastName: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    })
+    const handleSignupForm = ({ target: { name, value } }) => {
+        setSignupForm({ ...signupForm, [name]: value })
+    }
+
+    // POST request to add new user
+    const userSignup = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await axios.post('/api/user/signup', signupForm)
+            
+            authService.login(data.token)
+            
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    // Toggle signup button active state
+    const [buttonActive, setButtonActive] = useState(false)
+    useEffect(() => {
+        if (
+            signupForm.password.length >= 8 &&
+            signupForm.password === signupForm.confirmPassword
+        ) {
+            setButtonActive(true)
+        } else {
+            setButtonActive(false)
+        }
+
+    }, [signupForm])
+
     return (
         <div className={`bg-slate-50 flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24 ${toggleHidden ? 'hidden' : 'flex'}`}>
             <div className="mx-auto w-full max-w-sm lg:w-96">
@@ -10,7 +56,7 @@ export default function Signup({ toggleHidden, setToggleHidden }) {
 
                 <div className="mt-10">
                     <div>
-                        <form className="space-y-6">
+                        <form className="space-y-6" onSubmit={userSignup}>
                             <div className="space-y-6 sm:space-y-0 sm:flex sm:justify-between">
                                 <div>
                                     <label className="block text-sm font-medium leading-6 text-gray-900">
@@ -18,10 +64,12 @@ export default function Signup({ toggleHidden, setToggleHidden }) {
                                     </label>
                                     <div className="mt-2">
                                         <input
+                                            className="block w-full rounded-md outline-none border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                                             name="firstName"
                                             type="text"
                                             required
-                                            className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                                            value={signupForm.firstName}
+                                            onChange={handleSignupForm}
                                         />
                                     </div>
                                 </div>
@@ -32,10 +80,12 @@ export default function Signup({ toggleHidden, setToggleHidden }) {
                                     </label>
                                     <div className="mt-2">
                                         <input
+                                            className="block w-full rounded-md outline-none border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                                             name="lastName"
                                             type="text"
                                             required
-                                            className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                                            value={signupForm.lastName}
+                                            onChange={handleSignupForm}
                                         />
                                     </div>
                                 </div>
@@ -47,10 +97,12 @@ export default function Signup({ toggleHidden, setToggleHidden }) {
                                 </label>
                                 <div className="mt-2">
                                     <input
+                                        className="block w-full rounded-md  outline-none border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                                         name="username"
                                         type="text"
                                         required
-                                        className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                                        value={signupForm.username}
+                                        onChange={handleSignupForm}
                                     />
                                 </div>
                             </div>
@@ -61,10 +113,12 @@ export default function Signup({ toggleHidden, setToggleHidden }) {
                                 </label>
                                 <div className="mt-2">
                                     <input
+                                        className="block w-full rounded-md  outline-none border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                                         name="email"
                                         type="email"
                                         required
-                                        className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                                        value={signupForm.email}
+                                        onChange={handleSignupForm}
                                     />
                                 </div>
                             </div>
@@ -75,10 +129,12 @@ export default function Signup({ toggleHidden, setToggleHidden }) {
                                 </label>
                                 <div className="mt-2">
                                     <input
+                                        className="block w-full rounded-md  outline-none border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                                         name="password"
                                         type="password"
                                         required
-                                        className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                                        value={signupForm.password}
+                                        onChange={handleSignupForm}
                                     />
                                 </div>
                                 <label className="text-xs text-gray-500">
@@ -92,18 +148,30 @@ export default function Signup({ toggleHidden, setToggleHidden }) {
                                 </label>
                                 <div className="mt-2">
                                     <input
+                                        className="block w-full rounded-md  outline-none border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                                         name="confirmPassword"
                                         type="password"
                                         required
-                                        className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                                        value={signupForm.confirmPassword}
+                                        onChange={handleSignupForm}
                                     />
                                 </div>
+                                <label className={`text-xs text-red-500 ${signupForm.password === signupForm.confirmPassword && 'hidden'}`}>
+                                    Passwords do not match
+                                </label>
                             </div>
 
                             <div>
                                 <button
+                                    className={
+                                        `flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
+                                        ${buttonActive
+                                            ? 'bg-teal-600 hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600'
+                                            : 'bg-gray-400'
+                                        }`
+                                    }
                                     type="submit"
-                                    className="flex w-full justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+                                    disabled={!buttonActive ? true : false}
                                 >
                                     Sign Up
                                 </button>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from "axios"
 
 import Signup from "@/components/signup"
 
+import authService from '@/utils/authService'
+
 export default function Login() {
+    // Toggle between login and signup form
     const [toggleHidden, setToggleHidden] = useState(true)
+
+    // Handle user login form
+    const [loginForm, setLoginForm] = useState({
+        username: '',
+        password: '',
+    })
+    const handleLoginForm = ({ target: { name, value } }) => {
+        setLoginForm({ ...loginForm, [name]: value })
+    }
+
+    // POST request to log in user
+    const userLogin = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await axios.post('/api/user/login', loginForm)
+
+            authService.login(data.token)
+
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
 
     return (
         <div className="flex min-h-full flex-1">
@@ -18,17 +45,19 @@ export default function Login() {
 
                     <div className="mt-10">
                         <div>
-                            <form className="space-y-6">
+                            <form className="space-y-6" onSubmit={userLogin}>
                                 <div>
                                     <label className="block text-sm font-medium leading-6 text-gray-900">
                                         Username
                                     </label>
                                     <div className="mt-2">
                                         <input
+                                            className="block w-full rounded-md outline-none border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                                             name="username"
                                             type="text"
                                             required
-                                            className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                                            value={loginForm.username}
+                                            onChange={handleLoginForm}
                                         />
                                     </div>
                                 </div>
@@ -39,38 +68,20 @@ export default function Login() {
                                     </label>
                                     <div className="mt-2">
                                         <input
+                                            className="block w-full rounded-md outline-none border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
                                             name="password"
                                             type="password"
                                             required
-                                            className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+                                            value={loginForm.password}
+                                            onChange={handleLoginForm}
                                         />
                                     </div>
                                 </div>
 
-                                {/* <div className="flex items-center justify-between">
-                                    <div className="flex items-center">
-                                        <input
-                                            id="remember-me"
-                                            name="remember-me"
-                                            type="checkbox"
-                                            className="h-4 w-4 rounded border-gray-300 text-teal-600 focus:ring-teal-600"
-                                        />
-                                        <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-700">
-                                            Remember me
-                                        </label>
-                                    </div>
-
-                                    <div className="text-sm leading-6">
-                                        <a href="#" className="font-semibold text-teal-600 hover:text-teal-500">
-                                            Forgot password?
-                                        </a>
-                                    </div>
-                                </div> */}
-
                                 <div>
                                     <button
+                                        className='flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm bg-teal-600 hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600'
                                         type="submit"
-                                        className="flex w-full justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
                                     >
                                         Sign in
                                     </button>
