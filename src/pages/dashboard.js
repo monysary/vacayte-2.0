ed82a168dc4b40434eff0213348478c790f90cdb
monysary <sary.mony@gmail.com>
2023-05-12 15:34:23 -0700

    updated dashboard side panel ui and wrote js for selecting each options

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,15 +1,14 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { Dialog, Menu, Transition } from '@headlessui/react'
 import {
     Bars3Icon,
     BellIcon,
-    CalendarIcon,
-    ChartPieIcon,
+    CalendarDaysIcon,
     Cog6ToothIcon,
-    DocumentDuplicateIcon,
-    FolderIcon,
+    NewspaperIcon,
+    BookOpenIcon,
     HomeIcon,
-    UsersIcon,
+    BuildingStorefrontIcon,
     XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
@@ -19,19 +18,12 @@ import Itinerary from '@/components/itinerary'
 import Activities from '@/components/activities'
 import CreateTrip from '@/components/create-trip'
 
-const navigation = [
-    { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-    { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-]
-const teams = [
-    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
-]
+import authService from '@/utils/authService'
+import Link from 'next/link'
+
+
+
+
 const userNavigation = [
     { name: 'Your profile', href: '#' },
     { name: 'Sign out', href: '#' },
@@ -42,11 +34,88 @@ function classNames(...classes) {
 }
 
 export default function Dashboard() {
+    useEffect(() => {
+        if (authService.loggedIn() && !authService.tokenExpired()) {
+            return
+        } else {
+            window.location.assign('/login')
+        }
+    }, [])
+
     const [sidebarOpen, setSidebarOpen] = useState(false)
+
+    // Navigation Items
+    const [dashboardNavigation, setDashboardNavigation] = useState([
+        { name: 'Home', href: '#', icon: HomeIcon, current: true },
+        { name: 'Calendar', href: '#', icon: CalendarDaysIcon, current: false },
+    ])
+    const [manageTripNavigation, setManageTripNavigation] = useState([
+        { name: 'Activities', href: '#', icon: BuildingStorefrontIcon, current: false },
+        { name: 'Create Itinerary', href: '#', icon: BookOpenIcon, current: false },
+        { name: 'View Itinerary', href: '#', icon: NewspaperIcon, current: false },
+    ])
+    const [yourTripNavigation, setYourTripNavigation] = useState([
+        { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
+        { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
+        { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+    ])
+
+    const handleNavigation = (navigation, item) => {
+        switch (navigation) {
+            case 'dashboardNavigation':
+                setDashboardNavigation(
+                    dashboardNavigation.map((object) => {
+                        if (object === item) {
+                            return { ...object, current: true }
+                        } else {
+                            return { ...object, current: false }
+                        }
+                    })
+                )
+                setManageTripNavigation(
+                    manageTripNavigation.map((object) => {
+                        return { ...object, current: false }
+                    })
+                )
+                break;
+
+            case 'manageTripNavigation':
+                setManageTripNavigation(
+                    manageTripNavigation.map((object) => {
+                        if (object === item) {
+                            return { ...object, current: true }
+                        } else {
+                            return { ...object, current: false }
+                        }
+                    })
+                )
+                setDashboardNavigation(
+                    dashboardNavigation.map((object) => {
+                        return { ...object, current: false }
+                    })
+                )
+                break;
+
+            case 'yourTripNavigation':
+                setYourTripNavigation(
+                    yourTripNavigation.map((object) => {
+                        if (object === item) {
+                            return { ...object, current: true }
+                        } else {
+                            return { ...object, current: false }
+                        }
+                    })
+                )
+                break;
+            default:
+                break;
+        }
+
+    }
 
     return (
         <>
-            <div>
+            <div className="bg-slate-50">
                 <Transition.Root show={sidebarOpen} as={Fragment}>
                     <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
                         <Transition.Child
@@ -88,6 +157,7 @@ export default function Dashboard() {
                                             </button>
                                         </div>
                                     </Transition.Child>
+
                                     {/* Sidebar component, swap this element with another sidebar if you like */}
                                     <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-teal-600 px-6 pb-4">
                                         <div className="flex h-16 shrink-0 items-center">
@@ -101,7 +171,7 @@ export default function Dashboard() {
                                             <ul role="list" className="flex flex-1 flex-col gap-y-7">
                                                 <li>
                                                     <ul role="list" className="-mx-2 space-y-1">
-                                                        {navigation.map((item) => (
+                                                        {dashboardNavigation.map((item) => (
                                                             <li key={item.name}>
                                                                 <a
                                                                     href={item.href}
@@ -128,7 +198,7 @@ export default function Dashboard() {
                                                 <li>
                                                     <div className="text-xs font-semibold leading-6 text-teal-200">Your teams</div>
                                                     <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                                        {teams.map((team) => (
+                                                        {yourTripNavigation.map((team) => (
                                                             <li key={team.name}>
                                                                 <a
                                                                     href={team.href}
@@ -173,18 +243,19 @@ export default function Dashboard() {
                 <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
                     {/* Sidebar component, swap this element with another sidebar if you like */}
                     <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-teal-600 px-6 pb-4">
-                        <div className="flex h-16 shrink-0 items-center">
+                        <Link href='/' className="flex h-16 shrink-0 items-center">
                             <img
                                 className="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=white"
                                 alt="Your Company"
                             />
-                        </div>
+                        </Link>
                         <nav className="flex flex-1 flex-col">
                             <ul role="list" className="flex flex-1 flex-col gap-y-7">
                                 <li>
-                                    <ul role="list" className="-mx-2 space-y-1">
-                                        {navigation.map((item) => (
+                                    <div className="text-xs font-semibold leading-6 text-gray-200">Dashboard</div>
+                                    <ul role="list" className="-mx-2 mt-2 space-y-1">
+                                        {dashboardNavigation.map((item) => (
                                             <li key={item.name}>
                                                 <a
                                                     href={item.href}
@@ -194,6 +265,7 @@ export default function Dashboard() {
                                                             : 'text-teal-200 hover:text-white hover:bg-teal-700',
                                                         'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                                                     )}
+                                                    onClick={() => handleNavigation('dashboardNavigation', item)}
                                                 >
                                                     <item.icon
                                                         className={classNames(
@@ -209,23 +281,52 @@ export default function Dashboard() {
                                     </ul>
                                 </li>
                                 <li>
-                                    <div className="text-xs font-semibold leading-6 text-teal-200">Your teams</div>
+                                    <div className="text-xs font-semibold leading-6 text-gray-200">Manage Trip</div>
                                     <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                        {teams.map((team) => (
-                                            <li key={team.name}>
+                                        {manageTripNavigation.map((item) => (
+                                            <li key={item.name}>
                                                 <a
-                                                    href={team.href}
+                                                    href={item.href}
                                                     className={classNames(
-                                                        team.current
+                                                        item.current
                                                             ? 'bg-teal-700 text-white'
                                                             : 'text-teal-200 hover:text-white hover:bg-teal-700',
                                                         'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                                                     )}
+                                                    onClick={() => handleNavigation('manageTripNavigation', item)}
+                                                >
+                                                    <item.icon
+                                                        className={classNames(
+                                                            item.current ? 'text-white' : 'text-teal-200 group-hover:text-white',
+                                                            'h-6 w-6 shrink-0'
+                                                        )}
+                                                        aria-hidden="true"
+                                                    />
+                                                    {item.name}
+                                                </a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                                <li>
+                                    <div className="text-xs font-semibold leading-6 text-gray-200">Your Trips</div>
+                                    <ul role="list" className="-mx-2 mt-2 space-y-1">
+                                        {yourTripNavigation.map((item) => (
+                                            <li key={item.name}>
+                                                <a
+                                                    href={item.href}
+                                                    className={classNames(
+                                                        item.current
+                                                            ? 'bg-teal-700 text-white'
+                                                            : 'text-teal-200 hover:text-white hover:bg-teal-700',
+                                                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                                    )}
+                                                    onClick={() => handleNavigation('yourTripNavigation', item)}
                                                 >
                                                     <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-teal-400 bg-teal-500 text-[0.625rem] font-medium text-white">
-                                                        {team.initial}
+                                                        {item.initial}
                                                     </span>
-                                                    <span className="truncate">{team.name}</span>
+                                                    <span className="truncate">{item.name}</span>
                                                 </a>
                                             </li>
                                         ))}
@@ -248,6 +349,7 @@ export default function Dashboard() {
                     </div>
                 </div>
 
+                {/* Search Bar area */}
                 <div className="lg:pl-72">
                     <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
                         <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
