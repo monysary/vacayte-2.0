9a44e29ebd3b1fe2a7ed7a76b37fd48e999c861e
monysary <sary.mony@gmail.com>
2023-05-24 14:19:30 -0700

    created route for adding a yelpID to saved activities of a trip

--- src/components/activities.js
+++ src/components/activities.js
@@ -59,22 +59,22 @@ export default function Activities({ activities }) {
                                     key={business.name}
                                     className="flex flex-col justify-between shadow-md divide-y divide-gray-200 rounded-lg bg-white border border-blue-200 text-center"
                                 >
-                                    <div className="flex flex-col p-3">
-                                        <a href={business.url} target='_blank'>
+                                    <a href={business.url} target='_blank'>
+                                        <div className="flex flex-col p-3">
                                             <img className="w-[180px] max-w-none aspect-square rounded-xl" src={business.image_url} alt="yelp business" />
-                                        </a>
-                                        <div className='flex justify-between items-start'>
-                                            <h3 className="mt-2 text-sm text-left font-medium text-gray-900">
-                                                {business.name}
-                                            </h3>
-                                            <div className="whitespace-nowrap rounded-full ml-2 mt-2 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                                                {business.rating} ★
+                                            <div className='flex justify-between items-start'>
+                                                <h3 className="mt-2 text-sm text-left font-medium text-gray-900">
+                                                    {business.name}
+                                                </h3>
+                                                <div className="whitespace-nowrap rounded-full ml-2 mt-2 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                                                    {business.rating} ★
+                                                </div>
                                             </div>
+                                            <dl className="mt-1 flex flex-grow flex-col justify-between">
+                                                <dd className="text-xs text-left text-gray-500">{business.categories.map((category) => category.title).join(', ')}</dd>
+                                            </dl>
                                         </div>
-                                        <dl className="mt-1 flex flex-grow flex-col justify-between">
-                                            <dd className="text-xs text-left text-gray-500">{business.categories.map((category) => category.title).join(', ')}</dd>
-                                        </dl>
-                                    </div>
+                                    </a>
                                     <div className="flex divide-x divide-gray-200">
                                         <div className="flex w-0 flex-1">
                                             <button
--- src/pages/api/trip/userTrip/index.js
+++ src/pages/api/trip/userTrip/index.js
@@ -28,6 +28,29 @@ export default async function handler(req, res) {
                 }
                 break;
 
+            case 'POST':
+                try {
+                    const trip = await Trip.findById(req.body._id)
+                    if (!trip) {
+                        return res.status(404).json({ message: 'Trip not found' })
+                    }
+
+                    const activity = await trip.activities.find((item) => item.name === req.body.name)
+                    if (!activity) {
+                        return res.status(404).json({ message: 'Activity not found' })
+                    }
+
+                    activity.saved.push({ businessID: req.body.yelpID })
+
+                    await trip.save()
+
+                    res.status(200).json(trip)
+
+                } catch (err) {
+                    console.log(err);
+                    res.status(500).json({ message: 'Internal Server Error' })
+                }
+                break;
             default:
                 break;
         }
