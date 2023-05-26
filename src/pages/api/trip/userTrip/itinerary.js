06b66a16f127dc836e9e203399dc3000f135c18d
monysary <sary.mony@gmail.com>
2023-05-26 13:11:38 -0700

    created itinerary daily activity delete route

--- src/pages/api/trip/userTrip/itinerary.js
+++ src/pages/api/trip/userTrip/itinerary.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
         switch (req.method) {
             case 'GET':
                 try {
-                    const trip = await Trip.findById(req.body._id)
+                    const trip = await Trip.findById(req.query._id)
                     if (!trip) {
                         return res.status(404).json({ message: 'Trip not found' })
                     }
@@ -46,6 +46,9 @@ export default async function handler(req, res) {
                     }
 
                     const itineraryIndex = updatedItinerary.itinerary.findIndex((item) => new Date(item.date).toISOString() === new Date(req.body.date).toISOString())
+                    if (!itineraryIndex) {
+                        return res.status(404).json({ message: 'Itinerary date not found' })
+                    }
                     updatedItinerary.itinerary[itineraryIndex].dailyActivities.push({
                         businessID: req.body.businessID,
                         time: new Date(req.body.time).toISOString()
@@ -58,6 +61,38 @@ export default async function handler(req, res) {
                     console.log(err);
                     res.status(500).json({ message: 'Internal Server Error' })
                 }
+                break;
+
+            case 'DELETE':
+                try {
+                    const trip = await Trip.findById(req.query._id)
+                    if (!trip) {
+                        return res.status(404).json({ message: 'Trip not found' })
+                    }
+
+                    const updatedItinerary = await Itinerary.findById(trip.itinerary)
+                    if (!updatedItinerary) {
+                        return res.status(500).json({ message: 'Internal Server Error' })
+                    }
+
+                    const itineraryIndex = updatedItinerary.itinerary.findIndex((item) => new Date(item.date).toISOString() === new Date(req.query.date).toISOString())
+                    if (!itineraryIndex) {
+                        return res.status(404).json({ message: 'Itinerary date not found' })
+                    }
+                    const dailyActivitiesIndex = updatedItinerary.itinerary[itineraryIndex].dailyActivities.findIndex((item) => item.businessID === req.query.businessID)
+                    if (!dailyActivitiesIndex) {
+                        return res.status(404).json({ message: 'Yelp business not found' })
+                    }
+                    updatedItinerary.itinerary[itineraryIndex].dailyActivities.splice(dailyActivitiesIndex, 1)
+                    await updatedItinerary.save()
+
+                    res.status(200).json(updatedItinerary)
+
+                } catch (err) {
+                    console.log(err);
+                    res.status(500).json({ message: 'Internal Server Error' })
+                }
+
                 break;
 
             default:
